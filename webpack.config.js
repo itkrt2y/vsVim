57dc994ee2dc566375c001bf0dd90152c78944ec
Tatsuya Itakura <itkrt2y.591721200@gmail.com>
2019-09-24 14:55:21 +0900

    Always emit sourcemaps

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = (_, argv) => {
       filename: "extension.js",
       libraryTarget: "commonjs2"
     },
-    devtool: isProd ? "source-map" : "inline-source-map",
+    devtool: "source-map",
     externals: { vscode: "commonjs vscode" },
     resolve: { extensions: [".ts", ".js"] },
     module: { rules: [{ test: /\.ts$/, loader: "ts-loader" }] }
